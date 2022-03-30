875dd0de11e7fc484b7cfb2ac2a71ded29b69043
Ania <Ania@Karolts-MacBook-Pro.local>
2022-03-30 22:33:06 +0200

    code refactor

--- main.js
+++ main.js
@@ -6,99 +6,86 @@ const searchedPokemonImg = document.getElementById('searched-pokemon-img');
 const searchedPokemonType = document.getElementById('searched-pokemon-type');
 const searchedPokemonAbilities = document.getElementById('searched-pokemon-abilities');
 const searchedPokemonMoves = document.getElementById('searched-pokemon-moves');
+const notFound = document.getElementById('not_found');
+const buttonRandom = document.getElementById('fetch-random-pokemon');
 
 function renderPokemon (data) {
-    console.log(data)
-    document.getElementById('not_found').style.display = 'none';
+    notFound.style.display = 'none';
     
     searchedPokemon.textContent = data.name.toUpperCase();
+
     const pokemonImg = document.createElement('img');
-    if (data.sprites.front_default) {
+    if (data.sprites?.front_default) {
         pokemonImg.src = data.sprites.front_default;
-        searchedPokemonImg.replaceChildren(pokemonImg);
     } else {
         pokemonImg.src = 'images/questionmark.png';
-        searchedPokemonImg.replaceChildren(pokemonImg);
     }
-    let arrOfTypes = data.types.map(element => {
-        return element.type.name;
-    })
+    searchedPokemonImg.replaceChildren(pokemonImg);
+
+    let arrOfTypes = data.types.map(element => element.type.name)
     searchedPokemonType.textContent = arrOfTypes.join(', ');
 
-    let arrOfAbilities = data.abilities.map(element => {
-        return element.ability.name;
-    })
+    let arrOfAbilities = data.abilities.map(element => element.ability.name)
     searchedPokemonAbilities.textContent = arrOfAbilities.join(', ');
 
-    let arrOfMoves = data.moves.map(element => {
-        return element.move.name;
-    })
+    let arrOfMoves = data.moves.map(element => element.move.name)
     let arrOfMovesRandom = [];
-    let i = 0;
-    while (i < 10 && i < arrOfMoves.length) {
+    while (arrOfMovesRandom.length < 10 && arrOfMovesRandom.length < arrOfMoves.length) {
         const index = Math.floor(Math.random() * arrOfMoves.length);
         if (!arrOfMovesRandom.includes(arrOfMoves[index])) {
             arrOfMovesRandom.push(arrOfMoves[index]);
-            i++;
         }
     }
+
     const movesList = document.createElement('ul');
-    for (let i = 0; i < arrOfMovesRandom.length; i++) {
+    arrOfMovesRandom.forEach(move => {
         const element = document.createElement('li');
         movesList.append(element);
-        element.textContent = arrOfMovesRandom[i];
-    }
+        element.textContent = move;
+    });
     searchedPokemonMoves.replaceChildren(movesList);
 
     pokemonInfo.style.display = 'flex';
     searchedPokemon.scrollIntoView({behavior: "smooth"});
 }
 
+function responseHandler (response, errorMessage) {
+    if (response.ok) {
+        return response.json()
+        } else {
+            throw new Error(errorMessage);
+        }
+}
+
+function errorHandler (error) {
+    pokemonInfo.style.display = 'none';
+    notFound.textContent = error.message;
+    notFound.style.display = 'block';
+    notFound.scrollIntoView({behavior: "smooth"});
+}
+
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const searchInputValue = searchInput.value.toLowerCase().trim();
-    console.log(searchInputValue);
     const searchUrl = `https://pokeapi.co/api/v2/pokemon/${searchInputValue}`;
     fetch(searchUrl)
         .then((response) => {
-            if (response.ok) {
-            return response.json()
-            } else {
-                throw new Error('Pokemon not found :(');
-            }
+            return responseHandler(response, 'Pokemon not found :(');
         })
-        .catch(error => {
-            console.log(error.message);
-            pokemonInfo.style.display = 'none';
-            document.getElementById('not_found').textContent = 'Pokemon not found :(';
-            document.getElementById('not_found').style.display = 'block';
-            document.getElementById('not_found').scrollIntoView({behavior: "smooth"});
-        })
+        .catch(errorHandler)
         .then(renderPokemon);
 });
 
 
-
-
-const buttonRandom = document.getElementById('fetch-random-pokemon');
-
 buttonRandom.addEventListener('click', (event) => {
-    const randomPokemon = Math.floor(Math.random() * 898);
+    const numberOfPokemons = 898;
+    const randomPokemon = Math.floor(Math.random() * numberOfPokemons);
     const randomPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`;
 
-    fetch(randomPokemonUrl).then((response) => {
-        if (response.ok) {
-            return response.json()
-            } else {
-                throw new Error("Something's wrong :(");
-            }
-    })
-    .catch(error => {
-        console.log(error.message);
-        pokemonInfo.style.display = 'none';
-        document.getElementById('not_found').textContent = "Something's wrong :(";
-        document.getElementById('not_found').style.display = 'block';
-        document.getElementById('not_found').scrollIntoView({behavior: "smooth"});
-    })
+    fetch(randomPokemonUrl)
+        .then((response) => {
+            return responseHandler(response, "Something's wrong :(");
+        })
+    .catch(errorHandler)
     .then(renderPokemon);
 });
